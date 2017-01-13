c6b808b21f88de0cc92ebae97c9248f3694246a2
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-01-13 15:18:27 -0700

    reorganize some things; start fetching version and computing aliases

--- build.js
+++ build.js
@@ -1,17 +1,65 @@
 #!/usr/bin/env node
+"use strict";
 
 const yaml = require('node-yaml');
 const path = require('path');
 const fs = require('fs');
+const semver = require('semver');
 const fetcherDefs = require('./lib/fetchers.js');
 
-const config = yaml.readSync('config.yml');
+const config = yaml.readSync('main-config.yml');
 
 const libs = Object.getOwnPropertyNames(config);
 
-let fetchers = libs.reduce((obj, lib) => {
-    let source = config[lib].source;
+const dataSetOps = {};
 
+dataSetOps.forEach = function (fun) {
+    libs.forEach(each => {
+        fun(this[each]);
+    });
+};
+
+dataSetOps.map = function (fun) {
+    return libs.map(each => {
+        return fun(this[each]);
+    });
+};
+
+dataSetOps.promiseAll = function(fun) {
+    return Promise.all(
+        this.map(each => {
+            return Promise.resolve(fun(each));
+        })
+    ).then(() => {
+        return this;
+    });
+};
+
+//The core logic of this build is promise-based.  Each function should take in and resolve the complete data set.
+createDataSet()
+    .then(populateVersions)
+    .then(computeAliases)
+    .then(ds => {
+        console.log(JSON.stringify(ds, null, 2));
+    }, error => {
+        console.error(error);
+    })
+;
+
+function createDataSet() {
+    return Promise.resolve(libs.reduce((obj, lib) => {
+        let source = config[lib].source;
+        obj[lib] = {
+            source: source,
+            versions: [],
+            aliases: {},
+            fetcher: fetcherFor(source)
+        };
+        return obj;
+    }, Object.create(dataSetOps, {})));
+}
+
+function fetcherFor(source) {
     let parts = source.split(':', 2);
     if (parts.length !== 2) {
         throw `Invalid source: ${source}`;
@@ -20,14 +68,41 @@ let fetchers = libs.reduce((obj, lib) => {
     let sourceLocation = parts[1];
     switch (sourceType) {
         case 'github':
-            obj[lib] = new fetcherDefs.GithubFetcher(sourceLocation);
+            return new fetcherDefs.GithubFetcher(sourceLocation);
             break;
         default:
             throw `Unrecognized source type: ${sourceType}`;
     }
-    return obj;
-}, {});
+}
 
-console.log(fetchers);
+function populateVersions(dataSet) {
+    return dataSet.promiseAll(lib => {
+        return lib.fetcher.availableVersions()
+            .then(vers => {
+                lib.versions = vers;
+            });
+    });
+}
+
+function computeAliases(dataSet) {
+    return dataSet.promiseAll(lib => {
+        let verNums = lib.versions.map(each => each.ver).filter(semver.valid);
+        let desired = computeDesiredAliases(verNums);
+        lib.aliases = [...desired].reduce((obj, alias) => {
+            obj[alias] = semver.maxSatisfying(verNums, alias);
+            return obj;
+        }, {});
+    });
+
+    function computeDesiredAliases(versionNames) {
+       return versionNames.reduce((set, each) => {
+           let major = semver.major(each);
+           let minor = semver.minor(each);
+           set.add(`${major}`);
+           set.add(`${major}.${minor}`);
+           return set;
+       }, new Set());
+    }
+}
 
 
