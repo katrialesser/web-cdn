09c4b5e77fc140c2c39f107c14e40a183a9481c2
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-01-19 17:08:13 -0700

    We're downloading Tarballs now!

--- build.js
+++ build.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 "use strict";
 
+const proc = require('child_process');
 const yaml = require('node-yaml');
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const semver = require('semver');
 const fetcherDefs = require('./lib/fetchers.js');
 
@@ -11,27 +12,45 @@ const config = yaml.readSync('main-config.yml');
 
 const libs = Object.getOwnPropertyNames(config);
 
+const scratch = path.join(process.cwd(), '.tmp');
+const contentScratch = path.join(scratch, 'content');
+fs.emptyDirSync(scratch);
+if (proc.execSync('git worktree list').indexOf(contentScratch) !== -1) {
+    proc.execSync('git worktree prune');
+}
+
 const dataSetOps = {};
 
-dataSetOps.forEach = function (fun) {
-    libs.forEach(each => {
-        fun(this[each]);
-    });
+dataSetOps.forEachLib = function (fun) {
+    this.libs.forEach(fun);
 };
 
-dataSetOps.map = function (fun) {
-    return libs.map(each => {
-        return fun(this[each]);
-    });
+dataSetOps.mapLibs = function (fun) {
+    return this.libs.map(fun);
 };
 
-dataSetOps.promiseAll = function(fun) {
+dataSetOps.promiseAllLibs = function (fun) {
     return Promise.all(
-        this.map(each => {
-            return Promise.resolve(fun(each));
+        this.mapLibs(lib => {
+            return Promise.resolve(fun(lib));
         })
     ).then(() => {
         return this;
+    });
+};
+
+dataSetOps.forEachLibVersion = function (fun) {
+    this.libs.forEach(lib =>
+        lib.versions.forEach(ver => fun(lib, ver))
+    );
+};
+
+dataSetOps.promiseAllLibVersions = function (fun) {
+    return this.promiseAllLibs(lib => {
+        let vers = lib.versions;
+        return Promise.all(
+            vers.map(v => Promise.resolve(fun(v, lib)))
+        );
     });
 };
 
@@ -39,24 +58,41 @@ dataSetOps.promiseAll = function(fun) {
 createDataSet()
     .then(populateVersions)
     .then(computeAliases)
+    .then(populateConfigs)
+    .then(identityPromise(checkoutContent))
+    .then(loadContentManifest)
+    .then(findNecessaryUpdates)
+    .then(applyUpdates)
     .then(ds => {
-        console.log(JSON.stringify(ds, null, 2));
+        // console.log(JSON.stringify(ds, null, 2));
+        console.log(ds);
     }, error => {
         console.error(error);
     })
 ;
 
+function identityPromise(fun) {
+    return function (dataSet) {
+        return Promise.resolve(fun(dataSet))
+            .then(() => dataSet);
+    }
+}
+
 function createDataSet() {
-    return Promise.resolve(libs.reduce((obj, lib) => {
+    let obj = Object.create(dataSetOps, {});
+    obj.libs = libs.map(lib => {
         let source = config[lib].source;
-        obj[lib] = {
+        return {
+            id: lib,
             source: source,
             versions: [],
             aliases: {},
+            contentPath: path.join(contentScratch, lib),
             fetcher: fetcherFor(source)
         };
-        return obj;
-    }, Object.create(dataSetOps, {})));
+    });
+
+    return Promise.resolve(obj);
 }
 
 function fetcherFor(source) {
@@ -76,33 +112,147 @@ function fetcherFor(source) {
 }
 
 function populateVersions(dataSet) {
-    return dataSet.promiseAll(lib => {
+    return dataSet.promiseAllLibs(lib => {
         return lib.fetcher.availableVersions()
             .then(vers => {
                 lib.versions = vers;
+                lib.versions.forEach(v => v.contentPath = path.join(lib.contentPath, v.name));
             });
     });
 }
 
+function populateConfigs(dataSet) {
+    return dataSet.promiseAllLibs(lib => {
+        return Promise.all(
+            lib.versions.map(version => {
+                return lib.fetcher.fetchConfig(version.ref)
+                    .then(config => {
+                        version.config = config;
+                    });
+            })
+        ).then(() => {
+            let latest = lib.versions.find(each => each.name === lib.aliases.latest).config;
+            lib.display = {
+                name: latest.name,
+                description: latest.description,
+                docs: latest.docs
+            };
+        });
+    });
+}
+
 function computeAliases(dataSet) {
-    return dataSet.promiseAll(lib => {
+    return dataSet.promiseAllLibs(lib => {
         let verNums = lib.versions.map(each => each.name).filter(semver.valid);
         let desired = computeDesiredAliases(verNums);
         lib.aliases = [...desired].reduce((obj, alias) => {
             obj[alias] = semver.maxSatisfying(verNums, alias);
             return obj;
-        }, {});
+        }, {
+            latest: verNums.reduce((max, each) => semver.gt(each, max) ? each : max, "0.0.0")
+        });
     });
 
     function computeDesiredAliases(versionNames) {
-       return versionNames.reduce((set, each) => {
-           let major = semver.major(each);
-           let minor = semver.minor(each);
-           set.add(`${major}`);
-           set.add(`${major}.${minor}`);
-           return set;
-       }, new Set());
+        return versionNames.reduce((set, each) => {
+            let major = semver.major(each);
+            let minor = semver.minor(each);
+            set.add(`${major}.x.x`);
+            set.add(`${major}.${minor}.x`);
+            return set;
+        }, new Set());
     }
+}
+
+function checkoutContent() {
+    return new Promise((resolve, reject) =>
+        proc.exec(`git worktree add ${contentScratch} content`, {
+            cwd: scratch
+        }, (err) => {
+            if (err) reject(err);
+            else resolve();
+        }));
+}
+
+function loadContentManifest(dataSet) {
+    return new Promise((resolve, reject) => {
+        fs.readJson(path.join(contentScratch, 'manifest.json'), (err, manifest) => {
+            if (err) reject(err);
+            else {
+                dataSet.manifest = manifest;
+                resolve(dataSet);
+            }
+        });
+    });
+}
+
+function findNecessaryUpdates(dataSet) {
+    let manifest = dataSet.manifest;
+    let manifestLibs = manifest.libraries;
+    dataSet.forEachLibVersion((lib, version) => {
+        version.needsUpdate = needsUpdate(lib, version);
+    });
+    return Promise.resolve(dataSet);
+
+    function needsUpdate(lib, version) {
+        //First, see if the directories we need are there
+        let shaFile = path.join(version.contentPath, '.git-sha');
+        if (!fs.existsSync(version.contentPath) || !fs.existsSync(shaFile)) {
+            return true;
+        }
+        //Now, see if the git sha has changed since the last manifest update
+        let mLib = manifestLibs[lib.id];
+        if (!mLib) {
+            return true;
+        }
+        let mVer = mLib.versions.find(mv => mv.name === version.name);
+        if (!mVer) {
+            return true;
+        }
+        if (mVer.git_sha !== version.git_sha) {
+            return true;
+        }
+        let shaFromFile = fs.readFileSync(shaFile, 'utf8');
+        if (shaFromFile !== version.git_sha) {
+            return true;
+        }
+        return false;
+    }
+}
+
+function applyUpdates(dataSet) {
+    let updates = [];
+    dataSet.forEachLibVersion((lib, version) => {
+        if (!version.needsUpdate) return;
+
+        updates.push({
+            lib: lib,
+            version: version
+        });
+    });
+
+    let promises = updates.map(u =>
+        Promise.resolve(u)
+            .then(fetchTarball)
+    );
+
+    return Promise.all(promises);
+}
+
+function fetchTarball(update) {
+    let libPath = path.join(scratch, 'tarballs', update.lib.id);
+    update.tarpath = path.join(libPath, update.version.name + '.tgz');
+
+    return new Promise((resolve, reject) => {
+        fs.ensureDir(libPath, err => {
+            if (err) reject(err);
+            else resolve(update.tarpath);
+        });
+    }).then(() => {
+        return update.lib.fetcher.fetchTarball(update.version.tarball, update.tarpath);
+    }).then(() => {
+        return update;
+    });
 }
 
 
