dcc63377ebf6f698b1e74cb77c34fb44aa13d83f
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-01-27 09:31:38 -0700

    start work on file system creation

--- new-run.js
+++ new-run.js
@@ -3,15 +3,19 @@
 const CdnConfig = require('./lib/cdn-config');
 const fsp = require('fs-extra-p');
 
+/*
+ * Steps:
+ * - Load config
+ * - Build Filesystem
+ * - Push Filesystem
+ */
 CdnConfig.loadFromConfig({
     "demo-lib": {
         source: "github:ThatJoeMoore/byu-web-cdn-demo-lib"
     }
-})
-    .then(cfg => {
-        console.log(cfg);
-        return fsp.writeJson('./config.json', cfg);
-    })
-    .catch(err => {
-        console.error(err);
-    });
+}).then(cfg => {
+    console.log(cfg);
+    return fsp.writeJson('./config.json', cfg);
+}).catch(err => {
+    console.error(err);
+});
