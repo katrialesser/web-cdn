814ccf558cb7a76ec9a42193928d2ba8367691a9
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-01-31 12:05:41 -0700

    Abortive work on a write-tracking filesystem.
    This is being abandoned because it's so much simpler to just hash files
    before and after and figure out what's changed. However, this might be
    useful in the future, so let's keep it in the history.

--- new-run.js
+++ new-run.js
@@ -2,6 +2,16 @@
 
 const CdnConfig = require('./lib/cdn-config');
 const fsp = require('fs-extra-p');
+const path = require('path');
+const buildFilesystem = require('./lib/build-filesystem');
+const commitContent = require('./lib/commit-content');
+
+const SCRATCH_DIR_NAME = '.tmp';
+
+const scratchPath = path.join(process.cwd(), SCRATCH_DIR_NAME);
+const contentPath = path.join(scratchPath, 'content');
+const workPath = path.join(scratchPath, 'work');
+
 
 /*
  * Steps:
@@ -9,13 +19,24 @@ const fsp = require('fs-extra-p');
  * - Build Filesystem
  * - Push Filesystem
  */
-CdnConfig.loadFromConfig({
-    "demo-lib": {
-        source: "github:ThatJoeMoore/byu-web-cdn-demo-lib"
-    }
-}).then(cfg => {
+let configPromise =
+// CdnConfig.loadFromConfig({
+//     "demo-lib": {
+//         source: "github:ThatJoeMoore/byu-web-cdn-demo-lib"
+//     }
+// });
+    fsp.readJson('config.json').then(cfg =>
+        new CdnConfig(cfg.libs, cfg.contentInfo)
+    );
+
+
+configPromise.then(cfg => {
     console.log(cfg);
-    return fsp.writeJson('./config.json', cfg);
+    return fsp.writeJson('./config.json', cfg)
+        .then(() => cfg);
+}).then(cfg => {
+    return buildFilesystem(cfg, contentPath, workPath)
+        .then(() => commitContent(cfg, contentPath));
 }).catch(err => {
     console.error(err);
 });
