66442dbf7ccf628b0d1a7039b07d8aae73654bc4
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-02-07 00:47:12 -0700

    get webhooks working!

--- lambda/github-hook.js
+++ lambda/github-hook.js
@@ -3,28 +3,58 @@
  */
 "use strict";
 
-const yaml = require('node-yaml');
-const path = require('path');
-
-const runner = require('../runner');
-
 require('babel-polyfill');
 
 /**
  *
  * @returns {Promise}
  */
-exports.handler = function githubTrigger(event, context, callback) {
-    //TODO: Add IP Validation
-    console.log('Running build from Github Event', event);
+exports.handler = function githubTrigger(incoming, context, callback) {
+    console.log('Incoming event', incoming);
+    let {callerIp, eventType, eventId, eventBody} = incoming;
+
+    let repoName = eventBody.repository.full_name;
+    console.log(`Received Github event '${eventType}' ${eventId} from ${callerIp} for repo ${repoName}`);
+
+    if (eventType === 'ping') {
+        console.log('Just got a ping; bailing');
+        callback(null, {ran: false, reason: 'eventType === ping'});
+        return Promise.resolve();
+    } else if (eventType !== 'push') {
+        console.log(`Skipping event of type ${eventType} because it isn't a 'push'`);
+        callback(null, {ran: false, reason: 'eventType !== push'});
+        return Promise.resolve();
+    }
+
+    //TODO: Add Github IP validation - https://api.github.com/meta
+
     try {
+        //Note: these requires are being delayed in the name of fast startup times.
+        const yaml = require('node-yaml');
+        const path = require('path');
+
+        const util = require('../lib/util');
+
         yaml.readPromise(path.join(process.cwd(), 'main-config.yml'))
             .then(libs => {
                 console.log('Running with libs config', libs);
+                let repoIsRegistered = util.objectAsArray(libs).some(pair => {
+                    let {key, value} = pair;
+                    return value.source === 'github:' + repoName;
+                });
+                if (!repoIsRegistered) {
+                    let message = `Repository ${repoName} is not included in config`;
+                    console.log(message);
+                    callback(null, {ran: false, reason: message});
+                    return;
+                }
+
+                const runner = require('../runner');
+
                 return runner(libs)
+                    .then(() => console.log('Finished run'))
+                    .then(() => callback(null, {ran: true}));
             })
-            .then(() => console.log('Finished run'))
-            .then(() => callback(null, {done: true}))
             .catch(err => {
                 console.error('Execution error', err, err.stack);
                 callback(err)
@@ -35,3 +65,4 @@ exports.handler = function githubTrigger(event, context, callback) {
     }
     console.log('fired up promises');
 };
+
